56044eb50ae17f4aa5060cdeae72386474c45696
isaac <isaactonyloi@gmail.com>
2023-03-29 08:44:46 +0300

    first backend version

--- Gemfile
+++ Gemfile
@@ -1,10 +1,17 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "3.1.0"
+ruby "2.7.4"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.3"
+
+gem 'bcrypt'
+
+gem 'will_paginate'
+
+
+gem 'jwt'
 
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,83 @@
+class PostsController < ApplicationController
+    #before_action :set_post, only: [:show, :destroy, :add_comment, :like]
+    #before_action :set_current_user
+
+    # GET /posts
+    def index
+      @posts = Post.all
+      render json: @posts
+    end
+  
+    # GET /posts/1
+
+    def like_count
+        likes.count 
+    end
+
+    def show
+        @post = Post.includes(:comments, :likes).find(params[:id])
+
+        #@post = Post.find(params[:id])
+        #likes = @post.likes
+        #@comments = @post.comments.as_json(includes: :likes)
+        #render :json => @comments
+        render :json => @post
+    end
+      
+  
+    # POST /posts
+    def create
+      @post = Post.new(post_params)
+      if @post.save
+        redirect_to @post, notice: 'Post was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    # DELETE /posts/1
+    def destroy
+      @post.destroy
+      redirect_to posts_url, notice: 'Post was successfully destroyed.'
+    end
+  
+    # POST /posts/1/comments
+    def add_comment
+      @comment = @post.comments.new(comment_params)
+      @comment.user = current_user
+      if @comment.save
+        redirect_to @post, notice: 'Comment was successfully added.'
+      else
+        render :show
+      end
+    end
+  
+    # POST /posts/1/like
+    def like
+      @like = @post.likes.new(user: current_user)
+      if @like.save
+        redirect_to @post, notice: 'Post was successfully liked.'
+      else
+        render :show
+      end
+    end
+  
+    private
+
+    #def set_current_user
+        #@current_user = current_user
+    #end
+  
+    def set_post
+      @post = Post.find(params[:id])
+    end
+  
+    def post_params
+      params.require(:post).permit(:post_pic, :caption).merge(user: @current_user)
+    end 
+    
+    def comment_params
+      params.permit(:comment, :post_id, :user_id)
+    end
+  end
+  
